fd4689cdfe491b5ae93305869e7238dca782c537
Ryo Somatani <ryo_somatani@outlook.com>
2023-02-25 16:12:30 -0500

    add alert

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,32 @@
-function App() {
+import { Box, Alert, AlertTitle } from '@mui/material';
+
+const App = () => {
+  // use Redux for alert state
+  const sampleAlert = {
+    message: 'test alert!',
+    type: 'success',
+  };
+
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <Box>
+      {sampleAlert.message && (
+        <Alert
+          severity={sampleAlert.type}
+          sx={
+            {
+              // style here
+            }
+          }
+          onClose={() => {
+            // reset alert state here
+          }}
+        >
+          <AlertTitle>{sampleAlert.type}</AlertTitle>
+          {sampleAlert.message}
+        </Alert>
+      )}
+    </Box>
   );
-}
+};
 
 export default App;
