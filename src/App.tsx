9a3af50c68da87ee8c0aec302fe64b91858887a6
Isaac <shocker945516@gmail.com>
2023-03-09 16:35:31 +0800

    Changed the way of loading and added example into App

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,16 @@ import {
   CircularProgress,
   CssBaseline,
 } from '@mui/material';
+import { useSelector, useDispatch } from 'react-redux';
+import { setLoading } from './RTK/loadingSlice';
 
 const App = () => {
   // use Redux for loading state
-  const isLoading = true;
+  // const isLoading = true;
+  const isLoading = useSelector((state) => state.loading.status);
+  const loadingDispatch = useDispatch();
+  loadingDispatch(setLoading(true));
+  // console.log(isLoading);
 
   // use Redux for alert state
   const sampleAlert = {
--- src/RTK/loadingSlice.ts
+++ src/RTK/loadingSlice.ts
@@ -1,18 +1,18 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-const initialState = { loading: false };
+const initialState = { status: false };
 
 const loadingSlice = createSlice({
   name: 'loading',
   initialState,
   reducers: {
     setLoading: (state, action) => {
-      state.loading = action.payload;
+      state.status = action.payload;
     },
   },
 });
 
-// export const selectLoading = (state: { loading: boolean }) => state.loading;
+export const selectLoading = (state: { loading: boolean }) => state.loading;
 
 export const { setLoading } = loadingSlice.actions;
 
