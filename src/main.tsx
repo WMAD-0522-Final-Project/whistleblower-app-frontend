8fbea7b90ed6c4a59b663b20fc47513959ac11e7
Ryo Somatani <ryo_somatani@outlook.com>
2023-03-10 00:19:57 -0800

    add reset scss

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { Provider } from 'react-redux';
 import store from './RTK/store';
+import './styles/base/index.scss';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
