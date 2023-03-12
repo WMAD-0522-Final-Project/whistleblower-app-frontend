5f8e275427fa5cbaa044f0b114f180c5a543df77
Ryo Somatani <ryo_somatani@outlook.com>
2023-03-12 15:37:21 -0700

    admin: add redux slice for company data

--- src/RTK/store.ts
+++ src/RTK/store.ts
@@ -1,8 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import loadingReducer from './loadingSlice';
+import companyReducer from './companySlice';
 
 const store = configureStore({
-  reducer: { loading: loadingReducer },
+  reducer: { loading: loadingReducer, company: companyReducer },
 });
+
+export type StoreState = ReturnType<typeof store.getState>;
 
 export default store;
