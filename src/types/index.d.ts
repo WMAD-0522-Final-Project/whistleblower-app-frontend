d96874a99db79d83ef6ae02a93ed64c3f2b6c5f1
Ryo Somatani <ryo_somatani@outlook.com>
2023-03-15 14:35:00 -0700

    admin add claim list + list item components

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,3 +1,29 @@
 // add types & interfaces here!! (or in each component directory)
 
 export type SampleType = string;
+
+export interface Claim {
+  id: string;
+  submissionDate: string;
+  message: string;
+  labels: ClaimLabel[];
+  category: ClaimCategory;
+  members: {
+    userId: string;
+    avatarUrl: string;
+  }[];
+  chats: Chat[];
+}
+
+export type ClaimCategory = string;
+
+export type ClaimLabel = string;
+
+export interface Chat {
+  datetime: string;
+  user: {
+    id: string;
+    avatarUrl: string;
+  };
+  content: string;
+}
