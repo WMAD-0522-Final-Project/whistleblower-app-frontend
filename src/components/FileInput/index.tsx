e8b52561e23cdc391be9805345b716bfd1cf56f7
Ryo Somatani <ryo_somatani@outlook.com>
2023-03-30 15:08:27 -0700

    temp

--- src/components/FileInput/index.tsx
+++ src/components/FileInput/index.tsx
@@ -1,4 +1,4 @@
-import React, { ChangeEventHandler } from 'react';
+import React, { ChangeEventHandler, EventHandler } from 'react';
 import { Button } from '@mui/material';
 import AttachFileIcon from '@mui/icons-material/AttachFile';
 import { SxProps } from '@mui/material';
@@ -13,38 +13,46 @@ type Props = {
 
 const FileInput = ({ onChange, name, sx }: Props) => {
   const { companyData } = useSelector(selectCompanyData);
+  const [];
+
+  const handleChange: ChangeEventHandler<HTMLInputElement> = (e) => {
+    onChange();
+  };
 
   return (
     // need styling here
-    <Button
-      sx={{
-        backgroundColor: companyData.themeColors.tertiary,
-        borderRadius: '30px',
-        boxShadow: '1px 1px 1px 1px rgba(0,0,0,0.2)',
-        color: '#000',
-        textAlign: 'center',
-        padding: '2px 10px',
-        fontSize: '0.7rem',
-        textTransform: 'capitalize',
-        ...sx,
-      }}
-    >
-      <AttachFileIcon sx={{ fontSize: '1rem' }} />
-      Attach File
-      <input
-        type="file"
-        name={name}
-        style={{
-          opacity: '0',
-          position: 'absolute',
-          top: '0',
-          left: '0',
-          width: '100%',
-          height: '100%',
+    <>
+      <Button
+        sx={{
+          backgroundColor: companyData.themeColors.tertiary,
+          borderRadius: '30px',
+          boxShadow: '1px 1px 1px 1px rgba(0,0,0,0.2)',
+          color: '#000',
+          textAlign: 'center',
+          padding: '2px 10px',
+          fontSize: '0.7rem',
+          textTransform: 'capitalize',
+          ...sx,
         }}
-        onChange={onChange}
-      />
-    </Button>
+      >
+        <AttachFileIcon sx={{ fontSize: '1rem' }} />
+        Attach File
+        <input
+          type="file"
+          name={name}
+          style={{
+            opacity: '0.4',
+            position: 'absolute',
+            top: '0',
+            left: '0',
+            width: '100%',
+            height: '100%',
+          }}
+          onChange={handleChange}
+        />
+      </Button>
+      <p></p>
+    </>
   );
 };
 
