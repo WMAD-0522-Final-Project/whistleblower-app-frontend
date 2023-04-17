d712058f95d45b53ba0f12777da2456455f380e0
Isaac <shocker945516@gmail.com>
2023-04-16 19:42:37 -0700

    Upload Interface

--- src/components/CustomAvatar/index.tsx
+++ src/components/CustomAvatar/index.tsx
@@ -1,12 +1,53 @@
+import { Typography, Button, Box } from '@mui/material';
+
 import FileInput from '../FileInput';
+import useModal from '../../hooks/useModal';
 
-type Props = {};
+type Props = {
+  onClickYes: () => void;
+};
 
-const CustomAvatar = (props: Props) => {
+const outerBoxStyle = {
+  width: 250,
+  height: 250,
+  bgcolor: '#FFCB14',
+  boxShadow: 24,
+};
+
+const innerBoxStyle = {
+  width: 200,
+  height: 200,
+  border: '5px solid white',
+};
+
+const buttonStyle = {
+  bgcolor: '#F96A02',
+  color: 'white',
+  borderRadius: '25px',
+  borderStyle: 'none',
+  margin: '0 30px',
+  boxShadow:
+    '0px 3px 1px -2px rgba(0,0,0,0.2), 0px 2px 2px 0px rgba(0,0,0,0.14), 0px 1px 5px 0px rgba(0,0,0,0.12)',
+  '&:hover': {
+    borderStyle: 'none',
+    bgcolor: '#eab676',
+  },
+};
+
+const CustomAvatar = ({ onClickYes }: Props) => {
+  const { handleClose, Modal } = useModal();
+
   return (
-    <div>
+    <Modal outerBoxStyle={outerBoxStyle} innerBoxStyle={innerBoxStyle}>
+      <Box></Box>
       <FileInput name="avatar" text="Uploade Avatar Image" />
-    </div>
+      <Button variant="contained" sx={buttonStyle} onClick={onClickYes}>
+        Apply
+      </Button>
+      <Button variant="outlined" sx={buttonStyle} onClick={handleClose}>
+        Back
+      </Button>
+    </Modal>
   );
 };
 
