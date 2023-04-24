13a8a6649e9a525a827e75ed58a0f9b39a504efb
Isaac <shocker945516@gmail.com>
2023-04-23 17:16:45 -0700

    Finished

--- src/pages/AdminHome/index.tsx
+++ src/pages/AdminHome/index.tsx
@@ -20,35 +20,72 @@ import { motion } from 'framer-motion';
 import ConfirmationModal from '../../components/ConfirmationModal';
 import CustomBox from '../../components/CustomBox/CustomBox';
 import { DragDropContext } from 'react-beautiful-dnd';
-import axios from 'axios';
+import axios, { AxiosResponse } from 'axios';
 import { useMutation, useQuery } from '@tanstack/react-query';
 import getAuthorizationValue from '../../helpers/getAuthorizationValue';
 import { da } from 'date-fns/locale';
 
 type Props = {};
 
-type DndResult = {
+interface DndResult {
   destination: { droppableId: string; index: number };
   source: { droppableId: string; index: number };
-};
+}
 
-const removeFrom = (column: [], index: number) => {
+const removeFrom = (column: Claim[], index: number): [Claim, Claim[]] => {
   const output = [...column];
   const [removedItem] = output.splice(index, 1);
   return [removedItem, output];
 };
 
-const addTo = (column: [], index: number, item: object) => {
-  const output = [...column];
+const addTo = (column: Claim[], index: number, item: Claim) => {
+  const output: Claim[] = [...column];
   output.splice(index, 0, item);
   return output;
+};
+
+const getClaimData = (): Promise<AxiosResponse> => {
+  const authorizationValue = getAuthorizationValue();
+
+  return axios({
+    method: 'GET',
+    url: `${import.meta.env.VITE_BACKEND_URL}/api/claim/list`,
+    headers: {
+      Authorization: authorizationValue,
+    },
+  });
+};
+
+const putStatus = ({
+  claimId,
+  status,
+}: {
+  claimId: string;
+  status: string;
+}): Promise<AxiosResponse> => {
+  console.log('claimId', claimId);
+  console.log('status', status);
+
+  const authorizationValue = getAuthorizationValue();
+  return axios({
+    method: 'PUT',
+    url: `${
+      import.meta.env.VITE_BACKEND_URL
+    }/api/claim/${claimId}/changeStatus`,
+    headers: {
+      Authorization: authorizationValue,
+    },
+    data: {
+      status,
+    },
+  });
 };
 
 const AdminHome = (props: Props) => {
   const { companyData } = useSelector(selectCompanyData);
   const { Modal, handleOpen, handleClose } = useModal();
   const [query, setQuery] = useState('');
-  const [claims, setClaims] = useState<Partial<Claim>[] | null>(null);
+  const [claims, setClaims] = useState<Claim[] | null>(null);
   const [isModalWindow, setIsModalWindow] = useState<boolean>(false);
   const { context, setContext } = useAllContext();
   const newClaim = 'unHandled';
@@ -64,33 +101,16 @@ const AdminHome = (props: Props) => {
   // const [claims, setClaims] = useState<Partial<Claim>[]>([]);
   const matches = useMediaQuery((theme: Theme) => theme.breakpoints.up('lg'));
 
-  const getClaimData = () => {
-    const authorizationValue = getAuthorizationValue();
-
-    return axios({
-      method: 'GET',
-      url: `${import.meta.env.VITE_BACKEND_URL}/api/claim/list`,
-      headers: {
-        Authorization: authorizationValue,
-      },
-    });
-  };
-
   const claimQuery = useQuery({
     queryKey: ['getClaimData'],
     queryFn: getClaimData,
   });
-
   let fetchedClaims = claimQuery.data?.data.claims;
 
   useEffect(() => {
-    setClaims(fetchedClaims);
-    console.log(claims);
-  }, []);
-
-  useEffect(() => {
-    console.log(claims);
-  }, [claims]);
+    console.log('claimQuery.data', claimQuery.data);
+    claimQuery.data && setClaims(fetchedClaims);
+  }, [claimQuery.data]);
 
   useEffect(() => {
     if (claims !== null) {
@@ -134,9 +154,17 @@ const AdminHome = (props: Props) => {
     },
   ];
 
-  const handleOnDragEnd = function (result: DndResult) {
+  const statusMutation = useMutation({
+    mutationFn: putStatus,
+  });
+
+  useEffect(() => {
+    console.log('claims', claims);
+  }, [claims]);
+
+  const handleOnDragEnd = (result: DndResult) => {
     if (!result.destination) return;
-    let claimsCopy = [...claims];
+    let claimsCopy = [...claims!];
 
     const sourceColumn = claimsCopy.filter(
       (claim) => claim.status === result.source.droppableId
@@ -159,6 +187,8 @@ const AdminHome = (props: Props) => {
       removedClaim
     );
 
+    console.log('removedClaim', removedClaim);
+
     claimsCopy = [
       ...new Set([...claimsCopy, ...newDestinationColumn, ...removedColumn]),
     ];
@@ -167,122 +197,110 @@ const AdminHome = (props: Props) => {
 
     setClaims(claimsCopy);
 
-    const putStatus = (removedClaim: { id: string }) => {
-      const authorizationValue = getAuthorizationValue();
-
-      return axios({
-        method: 'PUT',
-        url: `${import.meta.env.VITE_BACKEND_URL}/api/claim/:${
-          removedClaim.id
-        }/changeStatus`,
-        data: { status: result.destination.droppableId },
-        headers: {
-          Authorization: authorizationValue,
-        },
-      });
-    };
-
-    useMutation({
-      mutationFn: putStatus,
+    statusMutation.mutate({
+      claimId: removedClaim._id,
+      status: result.destination.droppableId,
     });
   };
 
   return (
-    // TODO: temporary styling until Mateus's task is done
-    <>
-      <DragDropContext onDragEnd={handleOnDragEnd}>
-        <div style={{ position: 'relative' }}>
-          <div
-            style={{
-              position: 'absolute',
-              width: '100vw',
-              height: '100vh',
-            }}
-          >
-            <Box
-              sx={{
+    claims && (
+      // TODO: temporary styling until Mateus's task is done
+      <>
+        <DragDropContext onDragEnd={handleOnDragEnd}>
+          <div style={{ position: 'relative' }}>
+            <div
+              style={{
+                position: 'absolute',
+                width: '100vw',
                 height: '100vh',
-                marginTop: '-5%',
-                zIndex: '-1',
               }}
             >
-              {/* TODO: temporary claim data */}
-              {/* <ClaimChat chatData={sampleClaimDetail.chats} /> */}
-              {/* {claims && <MainWindow claim={claims[0]}></MainWindow>} */}
-              {claimQuery.isLoading && <>Loading...</>}
-              {claimQuery.isError && <>{claimQuery.error}</>}
-              {claims && (
-                <div
-                  style={
-                    matches
-                      ? {
-                          display: 'flex',
-                          justifyContent: 'space-around',
-                          alignItems: 'center',
-                          width: '100%',
-                          height: '100%',
+              <Box
+                sx={{
+                  height: '100vh',
+                  marginTop: '-5%',
+                  zIndex: '-1',
+                }}
+              >
+                {/* TODO: temporary claim data */}
+                {/* <ClaimChat chatData={sampleClaimDetail.chats} /> */}
+                {/* {claims && <MainWindow claim={claims[0]}></MainWindow>} */}
+                {claimQuery.isLoading && <>Loading...</>}
+                {claimQuery.isError && <>{claimQuery.error}</>}
+                {claims && (
+                  <div
+                    style={
+                      matches
+                        ? {
+                            display: 'flex',
+                            justifyContent: 'space-around',
+                            alignItems: 'center',
+                            width: '100%',
+                            height: '100%',
+                          }
+                        : {
+                            display: 'flex',
+                            flexDirection: 'column',
+                            justifyContent: 'space-around',
+                            alignItems: 'center',
+                            width: '100%',
+                            height: '90%',
+                          }
+                    }
+                  >
+                    {columns.map((column) => (
+                      <ClaimBox
+                        animate={
+                          !matches
+                            ? expandState === column.id
+                              ? { height: '70%' }
+                              : { height: '6%' }
+                            : { height: '70%' }
                         }
-                      : {
-                          display: 'flex',
-                          flexDirection: 'column',
-                          justifyContent: 'space-around',
-                          alignItems: 'center',
-                          width: '100%',
-                          height: '90%',
+                        transition={{ duration: 0.5 }}
+                        onHoverStart={(e) => {
+                          setExpandState(column.id);
+                        }}
+                        onTap={(e) => {
+                          setExpandState(column.id);
+                        }}
+                        width={column.width}
+                        height={column.height}
+                        label={column.label}
+                        id={column.id}
+                        sx={
+                          matches
+                            ? {}
+                            : {
+                                width: '95%',
+                                height: '10px',
+                                marginTop: '1%',
+                              }
                         }
-                  }
-                >
-                  {columns.map((column) => (
-                    <ClaimBox
-                      animate={
-                        !matches
-                          ? expandState === column.id
-                            ? { height: '70%' }
-                            : { height: '6%' }
-                          : { height: '70%' }
-                      }
-                      transition={{ duration: 0.5 }}
-                      onHoverStart={(e) => {
-                        setExpandState(column.id);
-                      }}
-                      onTap={(e) => {
-                        setExpandState(column.id);
-                      }}
-                      width={column.width}
-                      height={column.height}
-                      label={column.label}
-                      id={column.id}
-                      sx={
-                        matches
-                          ? {}
-                          : {
-                              width: '95%',
-                              height: '10px',
-                              marginTop: '1%',
-                            }
-                      }
-                      key={column.id}
-                      claims={claims.filter(
-                        (claim) => claim.status === column.id
-                      )}
-                    />
-                  ))}
-                </div>
-              )}
-            </Box>
-          </div>
+                        key={column.id}
+                        claims={claims.filter(
+                          (claim) => claim.status === column.id
+                        )}
+                      />
+                    ))}
+                  </div>
+                )}
+              </Box>
+            </div>
 
-          {modalClaim && (
-            <Modal
-              outerBoxStyle={{ maxWidth: '1200px' }}
-              innerBoxStyle={{ width: '100%', height: '100%' }}
-            >
-              <MainWindow claim={modalClaim}></MainWindow>
-            </Modal>
-          )}
-        </div>
-      </DragDropContext>
-    </>
+            {modalClaim && (
+              <Modal
+                outerBoxStyle={{ maxWidth: '1200px' }}
+                innerBoxStyle={{ width: '100%', height: '100%' }}
+              >
+                <MainWindow claim={modalClaim}></MainWindow>
+              </Modal>
+            )}
+          </div>
+        </DragDropContext>
+      </>
+    )
     // <Box sx={{ backgroundColor: '#fff', height: '100vh' }}>
     //   {/* TODO: temporary claim data */}
     //   <ClaimChat chatData={sampleClaimDetail.chats} />
