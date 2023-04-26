45eae01809ef9de015db989fd1c9472ef26a3aa5
Ryo Somatani <ryo_somatani@outlook.com>
2023-04-26 00:02:30 -0700

    connect with backend for claim categories

--- src/components/general/ClaimForm/index.tsx
+++ src/components/general/ClaimForm/index.tsx
@@ -27,6 +27,12 @@ interface NewClaimRequestBody {
   // file: File;
   isAnonymous: boolean;
 }
+interface CategoriesResponseData {
+  categories: {
+    _id: string;
+    name: string;
+  }[];
+}
 interface NewClaimResponseData {
   message: string;
   claim: { [key: string]: any };
@@ -70,7 +76,27 @@ const ClaimForm = (props: Props) => {
         Authorization: getAuthorizationValue(),
       },
     });
+  const getCategories = async (): Promise<CategoriesResponseData> => {
+    const authorizationValue = getAuthorizationValue();
+
+    const res = await axios({
+      method: 'GET',
+      url: `${import.meta.env.VITE_BACKEND_URL}/api/claim/category/list`,
+      headers: {
+        Authorization: authorizationValue,
+      },
+    });
+    return res.data;
   };
+
+  const { data: categoriesData } = useQuery({
+    queryKey: ['categories'],
+    queryFn: getCategories,
+    staleTime: 1000000,
+    onError: async (error) => {
+      console.log('failed to fetch categories', error);
+    },
+  });
 
   const newClaimMutation = useMutation({
     mutationFn: createNewClaim,
@@ -145,19 +171,21 @@ const ClaimForm = (props: Props) => {
               width: '100%',
             }}
           />
-          <SelectBoxLabel
-            placeholder="Choose category"
-            label="Category"
-            name="category"
-            options={sampleClaimCategories}
-            color={companyData.themeColors.primary}
-            selectBoxSx={{
-              '& .MuiSelect-select': {
-                padding: '1rem',
-              },
-            }}
-            sx={{ mt: '1rem', width: '100%' }}
-          />
+          {categoriesData && (
+            <SelectBoxLabel
+              placeholder="Choose category"
+              label="Category"
+              name="category"
+              options={categoriesData.categories}
+              color={companyData.themeColors.primary}
+              selectBoxSx={{
+                '& .MuiSelect-select': {
+                  padding: '1rem',
+                },
+              }}
+              sx={{ mt: '1rem', width: '100%' }}
+            />
+          )}
           <TextareaLabel
             placeholder="Enter message"
             label="Message"
