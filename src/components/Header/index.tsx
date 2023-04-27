dc82611f5c086ceb5d3de2f65f24060d58443924
Ryo Somatani <ryo_somatani@outlook.com>
2023-04-27 13:16:23 -0700

    fix destination of app name link

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -59,7 +59,7 @@ const Header = ({ hasMenu = false }: Props) => {
       >
         <MuiLink
           component={RouterLink}
-          to="/admin"
+          to="/"
           sx={{
             textDecoration: 'none',
             '&:hover': {
