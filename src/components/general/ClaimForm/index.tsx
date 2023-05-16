711d349634ffbb6acf41f3dce5d691d04f6ca213
Ryo Somatani <ryo_somatani@outlook.com>
2023-05-15 19:00:21 -0700

    delete duplicate code for claim form

--- src/components/general/ClaimForm/index.tsx
+++ src/components/general/ClaimForm/index.tsx
@@ -101,28 +101,6 @@ const ClaimForm = (props: Props) => {
     });
   };
 
-  const getCategories = async (): Promise<CategoriesResponseData> => {
-    const authorizationValue = getAuthorizationValue();
-
-    const res = await axios({
-      method: 'GET',
-      url: `${import.meta.env.VITE_BACKEND_URL}/api/claim/category/list`,
-      headers: {
-        Authorization: authorizationValue,
-      },
-    });
-    return res.data;
-  };
-
-  const { data: categoriesData } = useQuery({
-    queryKey: ['categories'],
-    queryFn: getCategories,
-    staleTime: 1000000,
-    onError: async (error) => {
-      console.log('failed to fetch categories', error);
-    },
-  });
-
   const newClaimMutation = useMutation({
     mutationFn: createNewClaim,
     onSuccess: (data) => {
